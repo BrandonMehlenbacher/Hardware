a4c73c1150717be83ed775e2d9fee1c55267bdd1
Brandon <bmehlenbacher4@gmail.com>
2020-10-21 17:08:32 -0500

    10/21/2020

--- APD_Control/apd.py
+++ APD_Control/apd.py
@@ -35,28 +35,28 @@ class Ui_apdMonitor(object):
         self.apd_graph.setGeometry(QRect(40, 20, 461, 361))
         self.start = QPushButton(self.centralwidget)
         self.start.setObjectName(u"start")
-        self.start.setGeometry(QRect(560, 190, 131, 71))
+        self.start.setGeometry(QRect(560, 270, 131, 41))
         font = QFont()
         font.setPointSize(12)
         self.start.setFont(font)
         self.stop = QPushButton(self.centralwidget)
         self.stop.setObjectName(u"stop")
-        self.stop.setGeometry(QRect(560, 300, 131, 71))
+        self.stop.setGeometry(QRect(560, 320, 131, 51))
         self.stop.setFont(font)
         self.frequency = QDoubleSpinBox(self.centralwidget)
         self.frequency.setObjectName(u"frequency")
-        self.frequency.setGeometry(QRect(560, 140, 131, 31))
+        self.frequency.setGeometry(QRect(560, 230, 131, 31))
         self.frequency.setFont(font)
         self.frequency.setMaximum(10000.000000000000000)
         self.frequency.setSingleStep(0.100000000000000)
         self.frequency.setValue(10.000000000000000)
         self.label_frequency = QLabel(self.centralwidget)
         self.label_frequency.setObjectName(u"label_frequency")
-        self.label_frequency.setGeometry(QRect(560, 100, 131, 31))
+        self.label_frequency.setGeometry(QRect(560, 200, 131, 31))
         self.label_frequency.setFont(font)
         self.label_daqList = QLabel(self.centralwidget)
         self.label_daqList.setObjectName(u"label_daqList")
-        self.label_daqList.setGeometry(QRect(560, 20, 131, 31))
+        self.label_daqList.setGeometry(QRect(560, 130, 131, 31))
         self.label_daqList.setFont(font)
         self.daqList = QListWidget(self.centralwidget)
         QListWidgetItem(self.daqList)
@@ -69,16 +69,36 @@ class Ui_apdMonitor(object):
         QListWidgetItem(self.daqList)
         QListWidgetItem(self.daqList)
         self.daqList.setObjectName(u"daqList")
-        self.daqList.setGeometry(QRect(560, 60, 111, 31))
+        self.daqList.setGeometry(QRect(560, 160, 111, 31))
+        self.daqList.setFont(font)
         self.daqList.setSortingEnabled(False)
+        self.min_voltage = QDoubleSpinBox(self.centralwidget)
+        self.min_voltage.setObjectName(u"min_voltage")
+        self.min_voltage.setGeometry(QRect(560, 100, 131, 21))
+        self.min_voltage.setMinimum(-10.000000000000000)
+        self.min_voltage.setFont(font)
+        self.label_min_voltage = QLabel(self.centralwidget)
+        self.label_min_voltage.setObjectName(u"label_min_voltage")
+        self.label_min_voltage.setGeometry(QRect(560, 75, 141, 21))
+        self.label_min_voltage.setFont(font)
+        self.label_max_voltage = QLabel(self.centralwidget)
+        self.label_max_voltage.setObjectName(u"label_max_voltage")
+        self.label_max_voltage.setGeometry(QRect(560, 25, 141, 21))
+        self.label_max_voltage.setFont(font)
+        self.max_voltage = QDoubleSpinBox(self.centralwidget)
+        self.max_voltage.setObjectName(u"max_voltage")
+        self.max_voltage.setGeometry(QRect(560, 50, 131, 21))
+        self.max_voltage.setFont(font)
+        self.max_voltage.setValue(10)
         apdMonitor.setCentralWidget(self.centralwidget)
         self.menubar = QMenuBar(apdMonitor)
         self.menubar.setObjectName(u"menubar")
-        self.menubar.setGeometry(QRect(0, 0, 726, 26))
+        self.menubar.setGeometry(QRect(0, 0, 726, 21))
         apdMonitor.setMenuBar(self.menubar)
         self.statusbar = QStatusBar(apdMonitor)
         self.statusbar.setObjectName(u"statusbar")
         apdMonitor.setStatusBar(self.statusbar)
+
 
         self.retranslateUi(apdMonitor)
 
@@ -93,7 +113,8 @@ class Ui_apdMonitor(object):
         self.start.clicked.connect(self.start_acq)
         self.stop.clicked.connect(self.stop_acq)
         self.daqList.currentItemChanged.connect(self.change_value)
-        
+        self.max_voltage.valueChanged.connect(self.change_value)
+        self.min_voltage.valueChanged.connect(self.change_value)
     # setupUi
 
     def retranslateUi(self, apdMonitor):
@@ -125,19 +146,25 @@ class Ui_apdMonitor(object):
         ___qlistwidgetitem8.setText(QCoreApplication.translate("apdMonitor", u"Dev1/ai8", None));
         self.daqList.setSortingEnabled(__sortingEnabled)
 
+        self.label_min_voltage.setText(QCoreApplication.translate("apdMonitor", u"Minimum Voltage", None))
+        self.label_max_voltage.setText(QCoreApplication.translate("apdMonitor", u"Maximum Voltage", None))
+
     # retranslateUi
 
     def start_acq(self):
-        print(self.daqList.currentItem().text())
-        self._apd = APD_Reader(self.daqList.currentItem().text(),int(1000000/(self.frequency.value()/2)))
-        self._apd.start_acquisition()
-        self._timer = QTimer()
-        time = (1/self.frequency.value())*1000
-        self._timer.start(time)
-        self._timer.timeout.connect(self.graph_values)
-        self.start.setEnabled(False)
-        self.stop.setEnabled(True)
-        self._active = True
+        try:
+            print(self.daqList.currentItem().text())
+            self._apd = APD_Reader(self.daqList.currentItem().text(),int(1000000/(self.frequency.value()/2)),max_val = self.max_voltage.value(),min_val = self.min_voltage.value())
+            self._apd.start_acquisition()
+            self._timer = QTimer()
+            time = (1/self.frequency.value())*1000
+            self._timer.start(time)
+            self._timer.timeout.connect(self.graph_values)
+            self.start.setEnabled(False)
+            self.stop.setEnabled(True)
+            self._active = True
+        except AttributeError:
+            print("Make sure you selected a daq channel")
     def stop_acq(self):
         self._apd.stop_acquisition()
         self._apd.close_daq()
@@ -151,7 +178,7 @@ class Ui_apdMonitor(object):
             self._apd.stop_acquisition()
             self._apd.close_daq()
             self._apd = None
-            self._apd = APD_Reader(self.daqList.currentItem().text(),int(1000000/(self.frequency.value()/2)))
+            self._apd = APD_Reader(self.daqList.currentItem().text(),int(1000000/(self.frequency.value()/2)),max_val = self.max_voltage.value(),min_val = self.min_voltage.value(),)
             self._apd.start_acquisition()
     def graph_values(self):
         self.apd_graph.clear()
