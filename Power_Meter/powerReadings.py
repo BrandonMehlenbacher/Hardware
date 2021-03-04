dc8de40663061bfdf5ac8959d184c25662a5cb8c
Brandon <bmehlenbacher4@gmail.com>
2021-03-04 10:59:58 -0600

    Switching to numpy arrays

--- Power_Meter/powerReadings.py
+++ Power_Meter/powerReadings.py
@@ -1,11 +1,15 @@
 import pyvisa as visa
+import sys
 import numpy as np
 from ThorlabsPM100 import ThorlabsPM100
 import matplotlib.pyplot as plt
 import matplotlib
 import time
+from datetime import datetime
+import os
 import pyqtgraph as pg
-#matplotlib.use('TkAgg')
+
+
 def printResources(resourceManager):
     for  resource in resourceManager.list_resources():
         try:
@@ -13,46 +17,55 @@ def printResources(resourceManager):
             print(resource, '-->', rm.open_resource(resource).query('*IDN?').strip())
         except visa.VisaIOError:
             pass
-
-def update_line(myPlot,data):
-    myPlot.set_xdata(np.append(myPlot.get_xdata(),data[0]))
-    myPlot.set_xdata(np.append(myPlot.get_xdata(),data[1]))
-    plt.draw()
     
 class PowerMeter:
     def __init__(self,resource,duration='Endless'):
         self.resource = ThorlabsPM100(inst=resource)
         if duration == 'Endless':
-            self.duration = 1000000000
+            self.duration = 100000000
         else:
             self.duration = duration
         self.timeSpacing = 0.1
-        self.timeArray = []
-        self.values = []
+        self.valuesArray = np.zeros(shape = (2,int(self.duration/self.timeSpacing)+1))
     def run(self):
         currentTime = 0
         plt.xlabel("Time (s)")
         plt.ylabel("Power (mW)")
+        count = 0 
         while currentTime < self.duration:
             try:
                 currentValue = self.resource.read
                 currentTime += self.timeSpacing
-                self.timeArray.append(currentTime)
-                self.values.append(currentValue*1000)
-                #plt.scatter(currentTime,currentValue*1000)#,1,1,'.')
-                plt.plot(self.timeArray,self.values,c='k')
+                self.valuesArray[0,count] = currentTime
+                self.valuesArray[1,count] = currentValue*1000
+                plt.plot(self.valuesArray[0,:count],self.valuesArray[1,:count],c='k')
                 plt.pause(self.timeSpacing)
+                count +=1
             except KeyboardInterrupt:
                 plt.close()
+                sys.exit()
                 break
-    def save(self):
-        pass
+    def save(self,filename="data"):
+        directory = str(datetime.now().date())
+        new_trace = 1
+        if self.duration > 10000000:
+            print("I will not save this to a csv, I doubt it could handle that many values, also numpy is struggling to make that big of an array")
+            return
+        if not os.path.isdir(directory):
+                os.makedirs(directory)
+        new_file_description = directory+"/"+filename
+        new_file = new_file_description
+        while os.path.isfile(new_file+".csv"):
+            new_file = new_file_description+f"_{new_trace}"
+            new_trace +=1
+        np.savetxt(new_file+".csv", self.valuesArray.transpose(),delimiter = ',')
 if __name__ == "__main__":
     rm = visa.ResourceManager()
     #printResources(rm)
     resource = rm.open_resource('USB0::0x1313::0x8078::P0029177::INSTR')
-    powerMeter = PowerMeter(resource,100)
+    powerMeter = PowerMeter(resource, 1)
     powerMeter.run()
+    powerMeter.save(filename = "monitorRedLaser")
     
             
             
